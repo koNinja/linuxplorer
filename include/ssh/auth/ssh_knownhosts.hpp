93cf3048aa67388cbc23882ef465654380fcee17
koNinja <koninja1101@gmail.com>
2025-06-08 01:20:20 +0900

    [Fix] knownhosts logic

--- include/ssh/auth/ssh_knownhosts.hpp
+++ include/ssh/auth/ssh_knownhosts.hpp
@@ -1,21 +1,23 @@
 #ifndef SSH_KNOWNHOSTS_HPP
 #define SSH_KNOWNHOSTS_HPP
 
-#include <string_view>
-#include <memory>
 #include <ssh/ssh_session.hpp>
 #include <ssh/ssh_exception.hpp>
+#include <string_view>
+#include <memory>
 
 namespace linuxplorer::ssh::auth {
 	namespace internal {
-		struct ssh_knownhosts_ptr_delete {
+		struct ssh_knownhosts_delete {
 			void operator()(::LIBSSH2_KNOWNHOSTS* _ptr) const noexcept {
 				::libssh2_knownhost_free(_ptr);
 			}
 		};
+
+		using unique_ssh_knownhosts_ptr = std::unique_ptr<::LIBSSH2_KNOWNHOSTS, ssh_knownhosts_delete>;
 	}
 
-	enum class ssh_knownhosts_check_result {
+	enum class ssh_knownhosts_verify_result {
 		matched,
 		mismatch,
 		missing
@@ -24,38 +26,26 @@ namespace linuxplorer::ssh::auth {
 	constexpr const wchar_t* default_knownhosts_path = L"<default_path>";
 	
 	class ssh_knownhosts {
-		using ssh_knownhosts_ptr_t = std::unique_ptr<::LIBSSH2_KNOWNHOSTS, internal::ssh_knownhosts_ptr_delete>;
 		const ssh_session& m_session;
-		ssh_knownhosts_ptr_t m_knownhosts;
-		std::wstring m_knownhosts_path;
 
-		void write(::libssh2_knownhost* target) const;
+		internal::unique_ssh_knownhosts_ptr m_knownhosts;
+		std::wstring m_knownhosts_path;
 	public:
 		ssh_knownhosts(const ssh_session& host, std::wstring_view path = default_knownhosts_path);
 
 		ssh_knownhosts(const ssh_knownhosts&) = delete;
-		ssh_knownhosts& operator=(const ssh_knownhosts&) = delete;
-		ssh_knownhosts(ssh_knownhosts&&) = delete;
-		ssh_knownhosts& operator=(ssh_knownhosts&&) = delete;
+		ssh_knownhosts(ssh_knownhosts&&) = default;
 
-		void add(std::wstring_view comment = L"");
-		void remove();
-		ssh_knownhosts_check_result check() const;
+		void register_this(std::wstring_view comment = L"");
+		void unregister();
+		ssh_knownhosts_verify_result verify() const;
+
+		std::vector<ssh_address> enumerate() const;
+
+		void flush() const;
 
 		~ssh_knownhosts() = default;
 	};
-
-	class ssh_knownhost_exception : public ssh_exception {
-		protected:
-			ssh_knownhosts_check_result m_result;
-		public:
-			ssh_knownhost_exception(ssh_knownhosts_check_result result, const std::string& what) : ssh_exception(what), m_result(result) {};
-			ssh_knownhost_exception(ssh_knownhosts_check_result result, const char* what) : ssh_exception(what), m_result(result) {};
-
-			inline auth::ssh_knownhosts_check_result result() const noexcept { 
-				return this->m_result;
-			}
-		};
 }
 
 #endif // SSH_KNOWNHOSTS_HPP
