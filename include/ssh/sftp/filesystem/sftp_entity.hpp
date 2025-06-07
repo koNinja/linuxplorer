168e48fa9069480d2877bb7196f5fca6d3b435a1
koNinja <koninja1101@gmail.com>
2025-06-08 00:08:16 +0900

    [Add] Basic filesystem API

---
+++ include/ssh/sftp/filesystem/sftp_entity.hpp
@@ -0,0 +1,66 @@
+#ifndef SFTP_ENTITY_HPP
+#define SFTP_ENTITY_HPP
+
+#include <ssh/ssh_session.hpp>
+#include <ssh/sftp/sftp_session.hpp>
+#include <filesystem>
+#include <memory>
+
+namespace linuxplorer::ssh::sftp::filesystem {
+	class directory_entry {
+	private:
+		std::filesystem::path m_path;
+
+	public:
+		explicit directory_entry() = default;
+		explicit directory_entry(const std::filesystem::path& path);
+		directory_entry(const directory_entry& lhs) noexcept = default;
+		directory_entry(directory_entry&& rhs) noexcept = default;
+		
+		directory_entry& operator=(const directory_entry& lhs) noexcept = default;
+		directory_entry& operator=(directory_entry&& rhs) noexcept = default;
+
+		const std::filesystem::path& path() const noexcept;
+		operator const std::filesystem::path&() const noexcept;
+
+		virtual ~directory_entry() = default;
+	};
+
+	class directory_iterator {
+	public:
+		using iterator_category = std::input_iterator_tag;
+		using value_type = directory_entry;
+		using difference_type = std::ptrdiff_t;
+		using pointer = const directory_entry*;
+		using reference = const directory_entry&;
+		using pos_type = std::streampos;
+
+		directory_iterator() noexcept;
+		explicit directory_iterator(const sftp_session& session, const std::filesystem::path& path, std::filesystem::directory_options options = std::filesystem::directory_options::none);
+		directory_iterator(const directory_iterator&) noexcept = default;
+		directory_iterator(directory_iterator&& rhs) noexcept = default;
+
+		const value_type& operator*() const noexcept;
+		const value_type* operator->() const noexcept;
+		directory_iterator& operator++() noexcept;
+		bool operator==(const directory_iterator& itr) const noexcept;
+		bool operator!=(const directory_iterator& itr) const noexcept;
+
+		virtual ~directory_iterator();
+	private:
+		std::streampos m_pos;
+		std::shared_ptr<value_type[]> m_ptr;
+
+		std::uint32_t m_count;
+	};
+
+	inline directory_iterator begin(directory_iterator itr) noexcept {
+		return itr;
+	}
+
+	inline directory_iterator end(directory_iterator itr) noexcept {
+		return directory_iterator{};
+	}
+}
+
+#endif // SFTP_ENTITY_HPP
--- test/sshtest.cpp
+++ test/sshtest.cpp
@@ -1,10 +1,11 @@
 #include <gtest/gtest.h>
 
+#include <ssh/ssh_exception.hpp>
 #include <ssh/ssh_session.hpp>
 #include <ssh/sftp/sftp_session.hpp>
-#include <ssh/ssh_exception.hpp>
+#include <ssh/sftp/filesystem/sftp_manip.hpp>
+#include <ssh/sftp/filesystem/sftp_entity.hpp>
 #include <util/charset/multibyte_wide_compat_helper.hpp>
-#include <ssh/sftp/io/sftpstream.hpp>
 
 #include <fstream>
 #include <filesystem>
@@ -29,118 +30,11 @@ std::tuple<ssh::ssh_address, std::wstring, std::wstring> get_cert() {
 	);
 }
 
-/*
-TEST(ssh, exec) {
-	auto [addr, user, passwd] = get_cert();
-
-	ssh::ssh_session ss(addr);
-
-	ss.connect();
-	ss.authenticate(user, passwd);
-
-	::LIBSSH2_CHANNEL* channel = ::libssh2_channel_open_session(ss.get_session());
-	if (!channel) {
-		throw ssh::ssh_libssh2_exception(::libssh2_session_last_errno(ss.get_session()), "Failed to open a channel.");
-	}
-
-	int rc = ::libssh2_channel_exec(channel, "pwd");
-	if (rc < 0) {
-		throw ssh::ssh_libssh2_exception(rc, "Failed to request a shell on a chennel.");
-	}
-
-	::ssize_t nread;
-	do {
-		char buffer[0x1000];
-		nread = ::libssh2_channel_read(channel, buffer, sizeof(buffer));
-
-		::ssize_t i;
-		std::cout << "We read: " << std::endl;
-
-		for(i = 0; i < nread; ++i)
-			std::cout << buffer[i];
-
-		std::cout << std::endl;
-	} while(nread > 0);
-
-	::libssh2_channel_close(channel);
-	::libssh2_channel_free(channel);
-
-	ss.disconnect();
-}
-
-TEST(sftp, read) {
+TEST(sftp_manip, stat) {
+	const auto& [addr, user, pass] = get_cert();
 	using namespace linuxplorer;
 
-	auto [addr, user, passwd] = get_cert();
-
-	auto ss = new ssh::ssh_session(addr);
-
-	ss->connect();
-	ss->authenticate(user, passwd);
-
-	auto sftp = new ssh::sftp::sftp_session(*ss);
-	::LIBSSH2_SFTP_HANDLE *handle = ::libssh2_sftp_open(sftp->get_session(), "/home/koninja/libssh2-errno.csv", LIBSSH2_FXF_READ, 0);
-	if (handle == nullptr) {
-		throw ssh::ssh_libssh2_exception(::libssh2_sftp_last_error(sftp->get_session()), "Failed to open a file.");
-	}
-
-	auto hfile = new ssh::sftp::sftp_handle(*sftp, handle);
-
-	constexpr std::size_t buflen = 0x1000;
-
-	char buf[buflen];
-	::ssize_t bytes_read = ::libssh2_sftp_read(handle, buf, sizeof(buf));
-
-	for (int i = 0; i < 1477; i++) {
-		std::cout.put(buf[i]);
-	}
-
-	// flush
-	std::cout.flush();
-
-	auto weakref1 = ss->get_weak();
-	auto weakref2 = sftp->get_weak();
-	delete ss;
-	delete sftp;
-	delete hfile;
-
-	EXPECT_TRUE(weakref1.expired());
-	EXPECT_TRUE(weakref2.expired());
-}
-*/
-
-TEST(sftpstream, isftpstream) {
-	using namespace linuxplorer;
-
-	auto [addr, user, passwd] = get_cert();
-
 	ssh::ssh_session ss(addr);
-
 	ss.connect();
-	ss.authenticate(user, passwd);
-	
-	char data[0x1000];
-	ssh::sftp::io::isftpstream iss(ss, L"/home/koninja/libssh2-errno.csv");
-
-	std::string str;
-	while (std::getline(iss, str)) {
-		std::cout << str << std::endl;
-	}
-
-	ss.disconnect();
-}
-
-
-TEST(sftpstream, osftpstream) {
-	using namespace linuxplorer;
-
-	auto [addr, user, passwd] = get_cert();
-
-	ssh::ssh_session ss(addr);
-
-	ss.connect();
-	ss.authenticate(user, passwd);
-
-	ssh::sftp::io::osftpstream ofs(ss, L"/home/koninja/sample.txt");
-	ofs << "Hello, world!" << std::endl;
+	ss.authenticate(user, pass);
 }
